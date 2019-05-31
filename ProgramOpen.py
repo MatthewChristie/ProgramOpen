dd63c52f3e7da13ea5361d9146744c4d6dbef58e
Matthew Christie <matthew.c1998@hotmail.co.uk>
2019-06-01 00:01:41 +0100

    added loop to allow for mistypes. No error message added yet

--- ProgramOpen.py
+++ ProgramOpen.py
@@ -1,4 +1,4 @@
-# Program/macro Opener
+#Program/macro Opener
 #Matthew Christie
 #May 2019
 
@@ -18,29 +18,44 @@ atomlink = r'C:\Users\matth\AppData\Local\atom\atom.exe'
 chromelink = r'C:\Program Files (x86)\Google\Chrome\Application\chrome.exe'
 steamlink = r'C:\Program Files (x86)\Steam\Steam.exe'
 
-#Prompt for Input
-print('What would you like to open?')
-print('Programs: spotify, chrome, steam, word')
-print('Macros: coding, write, TBA, TBA')
-ProgramToOpen = input(">>> ")
+#Prompts for input (and loops until valid input recieved)
+repeat = True
 
-#Basic Program Openers
-if ProgramToOpen == "spotify":
-    subprocess.Popen(spotifylink)
-elif ProgramToOpen == "word":
-    subprocess.Popen(wordlink)
-elif ProgramToOpen == "steam"
-    subprocess.Popen(steamlink)
-elif ProgramToOpen == "chrome"
-    subprocess.Popen(chromelink)
+while repeat == True:
+    print('What would you like to open?')
+    print('Programs: spotify, chrome, steam, word')
+    print('Macros: coding, write, TBA, TBA')
+    ProgramToOpen = input(">>> ")
+
+    #Basic Program Openers
+    if ProgramToOpen == "spotify":
+        subprocess.Popen(spotifylink)
+        repeat = False
+    elif ProgramToOpen == "word":
+        subprocess.Popen(wordlink)
+        repeat = False
+    elif ProgramToOpen == "steam":
+        subprocess.Popen(steamlink)
+        repeat = False
+    elif ProgramToOpen == "chrome":
+        subprocess.Popen(chromelink)
+        repeat = False
 
 
-#Macro 1 - CODING
-if ProgramToOpen == "coding":
-    os.startfile(r'C:\Users\matth\Desktop\Programing\Python\Projects')
-    subprocess.Popen(atomlink)
+        #Macro 1 - CODING
+    if ProgramToOpen == "coding":
+        os.startfile(r'C:\Users\matth\Desktop\Programing\Python\Projects')
+        subprocess.Popen(atomlink)
+        repeat = False
 
-#Macro 2 - WRITE
-if ProgramToOpen == "write":
-    subprocess.Popen(wordlink)
-    subprocess.Popen(spotifylink)
+        #Macro 2 - WRITE
+    if ProgramToOpen == "write":
+        subprocess.Popen(wordlink)
+        subprocess.Popen(spotifylink)
+        repeat = False
+
+#Macro 3 -
+#if ProgramToOpen == "":
+
+#Macro 4 -
+#if ProgramToOpen == "":
